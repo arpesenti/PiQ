b8009fdd72f9c6bcb438f774f31f956b48748a58
Armando Pesenti Gritti <arpesenti@gmail.com>
2013-02-16 18:04:31 +0100

    Implemented Feet library.

--- libraries/Feet/Feet.cpp
+++ libraries/Feet/Feet.cpp
@@ -1,17 +1,71 @@
 #include "Feet.h"
 
-Feet::Feet(int leftServoPin, int rightServoPin) {
-
+Feet::Feet() {
+	areOpen = false;
 }
 
-void Feet::close() {
+void Feet::init() {
+	leftServo.attach(LEFT_SERVO_PIN);
+	rightServo.attach(RIGHT_SERVO_PIN);
+}
 
+void Feet::close(int speed) {
+	if (areOpen)
+	{
+		write(&leftServo, LEFT_CLOSED_ANGLE, speed);
+		write(&rightServo, RIGHT_CLOSED_ANGLE, speed);
+		areOpen = false;
+	}
+}
+
+void Feet::open(int speed) {
+	if (!areOpen)
+	{
+		write(&leftServo, LEFT_CLOSED_ANGLE - OPEN_ANGLE, speed);
+		write(&rightServo, RIGHT_CLOSED_ANGLE + OPEN_ANGLE, speed);
+		areOpen = true;
+	}
 }
 
 void Feet::open() {
+	open(100);
+}
 
+void Feet::close() {
+	close(100);
+}
+
+// move servo to angle in degree with speed in percentage
+void Feet::write(Servo *servo, int angle, int speed) {
+	int stepDelay;
+	if (speed > 100)
+	{
+		stepDelay = 0;
+	} else if (speed <= 0) {
+		stepDelay = 100;
+	} else {
+		stepDelay = 100 - speed;
+	}
+	int startingAngle = servo->read();
+	int step = (angle - startingAngle)/NUMBER_OF_STEPS; 
+	int currentAngle = startingAngle;
+	for (int i = 0; i<NUMBER_OF_STEPS; i++) {
+		servo->write(currentAngle);
+		currentAngle += step;
+		delay(stepDelay);
+	}
+	servo->write(angle); // if the step is not a divisor of the angle
 }
 
 void Feet::trick() {
-	
+	// TODO experiment a nice trick
+	randomSeed(analogRead(0));
+	int n = random(3,10);
+	if (areOpen) close();
+	for (int i=0; i<n; i++) {
+		open(random(50,100));
+		delay(random(100,200));
+		close(random(50,100));
+		delay(random(100,200));
+	}
 }
--- libraries/Feet/Feet.h
+++ libraries/Feet/Feet.h
@@ -1,15 +1,31 @@
 #ifndef Feet_h
 #define Feet_h
 
+#include <Arduino.h>
+#include <Servo.h>
+
+#define LEFT_SERVO_PIN 9
+#define RIGHT_SERVO_PIN 10
+#define OPEN_ANGLE 45
+#define LEFT_CLOSED_ANGLE 180
+#define RIGHT_CLOSED_ANGLE 0
+#define NUMBER_OF_STEPS 5
+
 class Feet {
 public:
-	Feet(int leftServoPin, int rightServoPin);
+	Feet();
+	void init();
 	void close();
 	void open();
+	void close(int speed);
+	void open(int speed);
 	void trick();
 	
 private:
-	
+	void write(Servo *servo, int angle, int speed);
+	bool areOpen;
+	Servo leftServo;
+	Servo rightServo;
 };
 
 #endif
