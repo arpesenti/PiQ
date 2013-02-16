0b2170b83090d534b028da754bd0802548112b9d
Armando Pesenti Gritti <arpesenti@gmail.com>
2013-02-16 18:27:49 +0100

    Implementation of Remote

--- libraries/Remote/Remote.cpp
+++ libraries/Remote/Remote.cpp
@@ -1,9 +1,27 @@
 #include "Remote.h"
 
-Remote::Remote(int IRPin) {
+Remote::Remote(): receiver(IR_PIN) {
+	currentStrategy = WAIT_STRATEGY;
+}
 
+void Remote::init() {
+	receiver.enableIRIn();
+}
+
+void Remote::update() {
+	if (receiver.decode(&results)) { // have we received an IR signal?
+		int value = results.value;
+		if (value == NORMAL_STRATEGY_BUTTON)
+		{
+			currentStrategy = NORMAL_STRATEGY;
+		} else if (value == WAIT_STRATEGY_BUTTON) {
+			currentStrategy = WAIT_STRATEGY;
+		} else if (value == PANIC_STRATEGY_BUTTON) {
+			currentStrategy = PANIC_STRATEGY;
+		}
+	} 
 }
 
 char Remote::strategy() {
-	
+	return currentStrategy;
 }
--- libraries/Remote/Remote.h
+++ libraries/Remote/Remote.h
@@ -1,12 +1,27 @@
 #ifndef Remote_h
 #define Remote_h
 
+#include <Arduino.h>
+#include <IRremote.h>
+
+#define IR_PIN 11
+#define WAIT_STRATEGY_BUTTON 0 //TODO remote code
+#define WAIT_STRATEGY 0
+#define NORMAL_STRATEGY_BUTTON 1 //TODO remote code
+#define NORMAL_STRATEGY 1
+#define PANIC_STRATEGY_BUTTON 2 //TODO remote code
+#define PANIC_STRATEGY 2
+
 class Remote {
 public:
-	Remote(int IRPin);
+	Remote();
+	void init();
 	char strategy();
+	void update(); // to be called every loop
 private:
-	
+	IRrecv receiver;
+	decode_results results;
+	char currentStrategy;
 };
 
 #endif
