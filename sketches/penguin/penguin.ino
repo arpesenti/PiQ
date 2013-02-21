404b3a5c583caf995d604c39a69bcc119908f5e2
Armando Pesenti Gritti <arpesenti@gmail.com>
2013-02-21 21:56:54 +0100

    A first implementation of the main sketch, Penguin.

--- sketches/penguin/penguin.ino
+++ sketches/penguin/penguin.ino
@@ -31,9 +31,16 @@
 #define DEPOSIT 10
 #define PANIC 11
 
+// Approach results
+// TODO move this constants in robot
+#define APPROACH_FOUND_LINE 1
+#define APPROACH_NOT_FOUND_LINE 2
+#define APPROACH_FAILED 3
+
 // Robot
 Robot robot;
 int state;
+int result;
 
 void setup() {
         Serial.begin(9600);
@@ -43,32 +50,81 @@ void setup() {
         robot.start();
         delay(3000);
         Serial.println("init");
-  
 }
 
 void loop() {
-	switch(state){
-		case WAIT: 
-                      break;
-			//update strategy
-		case EXPLORE_SCAN:
-			if(robot.scanForEgg())
-				changeStateTo(EXPLORE_REACHEGG);
-			else
-				changeStateTo(EXPLORE_CHANGE_POSITION);
-			break;
-                case EXPLORE_REACHEGG:
-                      Serial.println("start reach egg");
-                      delay(2000);
-                       Serial.println(robot.reachEgg());
-                        state = WAIT;
-                        break;
-		case PANIC:
-			break;
-						
-
-
-	} 
+  switch(state){
+    case WAIT: 
+      break;
+      //update strategy
+      
+    case EXPLORE_SCAN:
+      if(robot.scanForEgg())
+        changeStateTo(EXPLORE_REACHEGG);
+      else
+	changeStateTo(EXPLORE_CHANGE_POSITION);
+      break;
+      
+    case EXPLORE_REACHEGG:
+      if(robot.reachEgg())
+        changeStateTo(CATCH);
+      else
+        changeStateTo(EXPLORE_SCAN);
+      break;
+      
+    case CATCH:
+      if(robot.catchEgg())
+        changeStateTo(COMEBACK_POSITIONING);
+      else
+        changeStateTo(EXPLORE_SCAN);
+      break;
+      
+    case COMEBACK_POSITIONING:
+      if(robot.positioningTowardHome()) 
+        changeStateTo(COMEBACK_APPROACHING);
+      else 
+        // TODO: can not positioning toward home, what to do?  
+      break;
+      
+    case COMEBACK_APPROACHING:
+      result = robot.tryToApproach();
+      if (result == APPROACH_FOUND_LINE)
+        changeStateTo(COMEBACK_LINEFOLLOWING);
+      else if (result == APPROACH_NOT_FOUND_LINE)
+        changeStateTo(COMEBACK_LINESEARCHING);
+      else if (result == APPROACH_FAILED)
+        Serial.println("Approach failed");
+        //TODO what to do when failed?
+      break;
+      
+    case COMEBACK_LINEFOLLOWING:
+      if (robot.followLineToHome())
+        changeStateTo(DEPOSIT);
+      else
+        changeStateTo(COMEBACK_LINESEARCHING);
+      break;
+      
+    case COMEBACK_LINESEARCHING:
+      if (robot.searchLine())
+        changeStateTo(COMEBACK_LINEFOLLOWING);
+      else
+        //TODO what to do if it do not find the line?
+        Serial.println("Line not found");
+      break;
+      
+    case DEPOSIT:
+      if (robot.deposit()) {
+        robot.newInit();
+        changeStateTo(EXPLORE_SCAN);
+      } else 
+        //TODO what to do if can't deposit the egg?
+        Serial.println("Deposit failed");
+      break;    
+      
+    case PANIC:
+      Serial.println("Entered panic state");
+      break;
+  } 
 }
 
 
